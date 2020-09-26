f7ba36d46c4ebca0c5e6473a3ca4adec83cf65e9
Matt Watson <mattwat@gmail.com>
2020-09-26 18:51:26 -0400

    setup google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,24 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-63536741-2", // The property ID; the tracking code won't be generated without it
+        head: false, // Defines where to place the tracking script - `true` in the head and `false` in the body
+        anonymize: true, // Setting this parameter is optional
+        respectDNT: true, // Setting this parameter is also optional
+        exclude: ["/preview/**", "/do-not-track/me/too/"], // Avoids sending pageview hits from custom paths
+        pageTransitionDelay: 0, // Delays sending pageview hits on route update (in milliseconds)
+        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID", // Enables Google Optimize using your container Id
+        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID", // Enables Google Optimize Experiment ID
+        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID", // Set Variation ID. 0 for original 1,2,3....
+        defer: false, // Defers execution of google analytics script after page load
+        sampleRate: 100, // Any additional optional fields
+        siteSpeedSampleRate: 10,
+        cookieDomain: "michellescaraglino.com",
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `michelle-scaraglino`,
